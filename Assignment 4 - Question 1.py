7d3fc563205cec30bda3d6ed454af11f37138823
DualDustin <dualdustin@gmail.com>
2026-03-23 12:51:55 -0600

    Finish question 1 coding questions continue on with commenting questions

--- Assignment 4 - Question 1.py
+++ Assignment 4 - Question 1.py
@@ -1,5 +1,33 @@
 ### Question 1
 # 
 # 
+# 
 ###
 
+# importing
+import numpy
+import pandas
+from sklearn.datasets import load_breast_cancer
+breastCancerData = load_breast_cancer ()
+
+# feature matrix and target vector
+featureMatrix = breastCancerData.data # X
+targetVector = breastCancerData.target # y
+
+
+# getting the shape
+featureMatrixShape = featureMatrix.shape
+targetVectorShape = targetVector.shape
+
+print("Feature shape: " + str(featureMatrixShape))
+print("Target shape: " + str(targetVectorShape))
+
+
+# number of samples
+targetVectorCount = pandas.Series(targetVector).value_counts()
+
+print(targetVectorCount)
+
+
+# This dataset is not balanced since the proportion for class is not close to 50/50
+# it being 357/212 or 63%/37%
