58d8f0276ae47d694ed55269d615894bbd0ef73a
DualDustin <dualdustin@gmail.com>
2026-03-23 17:55:26 -0600

    Finish question 4 code to continue with assignment

---
+++ Assignment 4 - Question 4.py
@@ -0,0 +1,77 @@
+### Question 4
+# Use Neural Network
+#   Standardize input
+#   train model with at least 1 hidden layer, sigmoid output
+#   report train/test accuracy
+# 
+# why feature scaling is necessary
+# What an epoch is during enural network training
+###
+
+# setting up neural network model
+# importing
+from tensorflow.keras.models import Sequential
+neuralNetworkModel = Sequential()
+from tensorflow.keras.layers import Dense, InputLayer
+inputLayer = InputLayer(input_shape=(30,)) # input layer 2 neurons
+# hidden layer
+neuralNetworkModel.add(inputLayer) # first layer
+hiddenLayer = Dense(6)
+neuralNetworkModel.add(hiddenLayer)
+secondHiddenlayer = Dense(5) # second
+neuralNetworkModel.add(secondHiddenlayer)
+# output layer
+outputLayer = Dense(1, activation="sigmoid")
+neuralNetworkModel.add(outputLayer)
+# compile
+neuralNetworkModel.compile(loss="binary_crossentropy", metrics=["accuracy"])
+
+
+
+# training
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+
+from sklearn.datasets import load_breast_cancer
+breastCancerData = load_breast_cancer()
+
+featureMatrix = breastCancerData.data
+targetVector = breastCancerData.target
+
+
+# splitting
+features_train, features_test, labels_train, labels_test = train_test_split(featureMatrix, 
+                                                                            targetVector, 
+                                                                            test_size=0.2,
+                                                                            random_state=42)
+
+
+# standardize
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+features_train = scaler.fit_transform(features_train)   # learn mean/std from train
+features_test = scaler.transform(features_test)
+
+
+neuralNetworkModel.fit(features_train,labels_train,epochs=10)
+
+
+# Predict and accuracy of Train
+newDiseaseSamples = pd.DataFrame(features_train)
+classProbabilities = neuralNetworkModel.predict(newDiseaseSamples)
+
+loss, trainAccuracy = neuralNetworkModel.evaluate(features_train, labels_train)
+
+# Predict and accuracy of Test
+newDiseaseSamples = pd.DataFrame(features_test)
+classProbabilities = neuralNetworkModel.predict(newDiseaseSamples)
+
+loss, testAccuracy = neuralNetworkModel.evaluate(features_test, labels_test)
+
+
+print("\n\n")
+print("Train accuracy: " + str(trainAccuracy))
+print("Test accuracy: " + str(testAccuracy))
