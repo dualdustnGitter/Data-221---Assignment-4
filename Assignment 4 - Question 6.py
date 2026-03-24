13a1bdab0001b84805858f02718197fb53e3af5a
DualDustin <dualdustin@gmail.com>
2026-03-23 20:47:00 -0600

    Finish question 6.py

--- Assignment 4 - Question 6.py
+++ Assignment 4 - Question 6.py
@@ -25,19 +25,28 @@ print("train shape:", X_train.shape)
 # build cnn model
 model = models.Sequential([
     layers.Input(shape=(28, 28, 1)),
-    layers.Conv2D(16,3,padding="same", activation="relu"), # ReLU
-    layers.MaxPool2D(), # Pool
-    layers.Conv2D(32,3,padding="same", activation="relu"), # ReLU
-    layers.MaxPool2D(), # Pool
+    layers.Conv2D(16,3,padding="same", activation="relu"), # Conv2D layer with relu
+    layers.MaxPool2D(), # MaxPool2D layer
 ])
 model.add(layers.Flatten()) # Flatten
-model.add(layers.Dense(128, activation="relu")) # ReLU
+model.add(layers.Dense(128, activation="relu")) # Dense output layer
 model.add(layers.Dropout(0,3)) # Dropout for "anti-" overfitting
-model.add(layers.Dense(10, activation="softmax")) # Softmax
 
 model.summary()
 
 
 # compile and train
-model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"] ) # Metrics is important for us, we chose accuracy
-history = model.fit(X_train, y_train,validation_split=0.1,epochs=3, batch_size=64) # epochs is # of forward backwards
+model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"] )
+history = model.fit(X_train, y_train,validation_split=0.1,epochs=15, batch_size=64)
+loss, accuracy = model.evaluate(X_test, y_test)
+
+print("Accuracy of CNN: " + accuracy)
+
+
+# CNN's are meant to work on these images since it works with 2D matrices
+# while normal neural networks work on 1D vectors which we need to flatten
+# the image into which causes a whole dimension worth of features to be lost
+# which overall decreases accuracy
+
+# the convolution layer here is learning how to predict what actually is being shown
+# by looking at small patterns which allows it to classify different things in the image
