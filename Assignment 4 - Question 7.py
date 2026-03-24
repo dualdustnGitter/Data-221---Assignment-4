34ed94a37734746b2fcf4e46f906e65d72fde38a
DualDustin <dualdustin@gmail.com>
2026-03-23 21:17:42 -0600

    Finish question 7 to complete assignment

--- Assignment 4 - Question 7.py
+++ Assignment 4 - Question 7.py
@@ -23,37 +23,41 @@ X_test = X_test.astype("float32") / 255.0
 X_train = X_train[..., None]
 X_test = X_test[..., None]
 
-print("train shape:", X_train.shape)
+# print("train shape:", X_train.shape)
 
 
-# build cnn model
-model = models.Sequential([
-    layers.Input(shape=(28, 28, 1)),
-    layers.Conv2D(16,3,padding="same", activation="relu"), # Conv2D layer with relu
-    layers.MaxPool2D(), # MaxPool2D layer
-])
-model.add(layers.Flatten()) # Flatten
-model.add(layers.Dense(128, activation="relu")) # Dense output layer
-model.add(layers.Dropout(0,3)) # Dropout for "anti-" overfitting
+# # build cnn model
+# model = models.Sequential([
+#     layers.Input(shape=(28, 28, 1)),
+#     layers.Conv2D(16,3,padding="same", activation="relu"), # Conv2D layer with relu
+#     layers.MaxPool2D(), # MaxPool2D layer
+# ])
+# model.add(layers.Flatten()) # Flatten
+# model.add(layers.Dense(128, activation="relu")) # Dense output layer
+# model.add(layers.Dropout(0,3)) # Dropout for "anti-" overfitting
+# model.add(layers.Dense(10, activation="softmax"))
 
-model.summary()
-
-
-# compile and train
-model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"] )
-history = model.fit(X_train, y_train,validation_split=0.1,epochs=15, batch_size=64)
+# model.summary()
 
 
+# # compile and train
+# model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"] )
+# history = model.fit(X_train, y_train,validation_split=0.1,epochs=15, batch_size=64)
 
 
-# save and predict test
-model.save("mnist_model.h5")
 
+
+# # save and predict test
+# model.save("mnist_model.h5")
+
+import numpy
 from tensorflow.keras.models import load_model
 
 loaded_mnist_classifier_model = load_model("mnist_model.h5")
 testPredictions = loaded_mnist_classifier_model.predict(X_test)
 
+# convert prob's to class labels
+testPredictions = numpy.argmax(testPredictions, axis=1)
 
 
 # Confusion matrix
@@ -65,3 +69,25 @@ print(confusionMatrix)
 
 
 # misclassified images
+misclassifiedImagesIndices = numpy.where(testPredictions != y_test)[0]
+
+
+# show the misclassified images
+import matplotlib.pyplot as plt
+
+# source: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html
+for i in range(3):
+    index = misclassifiedImagesIndices[i]
+    
+    plt.imshow(X_test[index].squeeze(), cmap="gray")
+    print("True: " + str(y_test[index]))
+    print("Predicted: " + str(testPredictions[index]))
+    plt.show()
+
+
+
+# One common pattern i see in the misclassifications is that the 'True" label is a 
+# higher value then the predicted ones
+
+# a way to improve the model's performance is to increase the dataset size for it 
+# to train on.
